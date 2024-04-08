131373611b4ea225ccfd2c2bbafd282cb05675b5
Bo Li <lb284542@gmail.com>
2024-04-08 23:05:30 +0800

    handle last line

--- ToLogseq.js
+++ ToLogseq.js
@@ -2,7 +2,7 @@
 // @name         ToLogseq Format Converter for ChatGPT
 // @name:zh-CN   ToLogseq Markdown格式转换器 for ChatGPT
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.3
 // @description  Convert markdown text to Logseq formatted Markdown text, which is available for ChatGPT and other similar tools using md format.
 // @description:zh-cn 将 Markdown 文本转换为 Logseq 格式的 Markdown 文本，可用于 ChatGPT 和其他使用 md 格式的类似工具。
 // @author       Another_Ghost
@@ -37,7 +37,7 @@
             const clipboardText = await navigator.clipboard.readText(); // Read text from clipboard
             if(clipboardText.includes(mark))
             {
-                showAlert('Already converted to Logseq!');
+                showAlert('Already converted to Logseq!', 'lightorange');
             }
             else
             {
@@ -48,63 +48,17 @@
             }
         } catch (err) {
             console.error('Error converting to logseq:', err);
-            showAlert('Failed to access clipboard');
+            showAlert('Failed to access clipboard', 'lightcoral');
         }
     }
 
-    function ChatGPTToLogseq(text) {
-        
-        let lines = text.split('\n');
-        let output = '';
-        let bNewBlock = true;
-        for(let i=0; i<lines.length; i++) {
-            if(lines[i].match(/^#/))
-            {
-                if(lines[i].match(/^### \d\./))
-                {
-                    lines[i] = lines[i].replace(/^### (\d\.) /, '### ');
-                    lines[i] = lines[i] + '\n' + 'logseq.order-list-type:: number';
-                }
-                lines[i] = '- ' + lines[i];
-            }
-            else if(lines[i].match(/^\d+\./))
-            {
-                lines[i] = lines[i].replace(/^\d+\. /, '');
-                lines[i] = '    - ' + lines[i] + '\n' + 'logseq.order-list-type:: number';
-            }
-            else if (lines[i].match(/^- /))
-            {
-                lines[i] = lines[i].replace(/^- /, '    - ');
-            }
-            else if(bNewBlock)
-            {
-                lines[i] = '    - ' + lines[i];
-                bNewBlock = false;
-            }
-
-            if(lines[i].trim().match(/^\\\(/) && lines[i].trim().match(/\\\)$/))
-            {
-                lines[i] = lines[i].trim().replace(/^\\\(/, '$$').replace(/\\\)$/, '$$');
-            }
-            if(lines[i].trim() !== '')
-            {
-                output += lines[i] + '\n';
-            }
-            else
-            {
-                bNewBlock = true;
-            }
-        }
-        return output;
-    }
-
-    function showAlert(message) {
+    function showAlert(message, backgroundColor = 'lightgreen') {
         const alertBox = document.createElement('div');
         alertBox.textContent = message;
         alertBox.style.position = 'fixed';
         alertBox.style.bottom = '60px';
         alertBox.style.right = '20px';
-        alertBox.style.backgroundColor = 'lightblue';
+        alertBox.style.backgroundColor = 'lightgreen';
         alertBox.style.padding = '10px';
         alertBox.style.borderRadius = '5px';
         alertBox.style.zIndex = '1001';
@@ -117,3 +71,60 @@
     }
 })();
 
+function ChatGPTToLogseq(text) {
+        
+    let lines = text.split('\n');
+    text = text.replace(/\n\n/g, '\n');
+    let output = '';
+    //let bNewBlock = false;
+    //let bCode = false;
+    for(let i=0; i<lines.length - 1; i++) {
+        // if(!bCode){
+        //     if(lines[i].includes('```'))
+        //     {
+        //         bCode = !bCode;
+        //     }
+        if(lines[i].match(/^#/))
+        {
+            if(lines[i].match(/^### \d\./))
+            {
+                lines[i] = lines[i].replace(/^### (\d\.) /, '### ');
+                lines[i] = lines[i] + '\n' + 'logseq.order-list-type:: number';
+            }
+            lines[i] = '- ' + lines[i];
+        }
+        else if(lines[i].match(/^\d+\./))
+        {
+            lines[i] = lines[i].replace(/^\d+\. /, '');
+            lines[i] = '    - ' + lines[i] + '\n' + 'logseq.order-list-type:: number';
+        }
+        else if (lines[i].match(/^- /))
+        {
+            lines[i] = lines[i].replace(/^- /, '    - ');
+        }
+        // else if(bNewBlock)
+        // {
+        //     lines[i] = '    - ' + lines[i];
+        //     bNewBlock = false;
+        // }
+
+        if(lines[i].trim().match(/^\\\(/) && lines[i].trim().match(/\\\)$/))
+        {
+            lines[i] = lines[i].trim().replace(/^\\\(/, '$$').replace(/\\\)$/, '$$');
+        }
+        if(lines[i].trim() !== '')
+        {
+            output += lines[i] + '\n';
+        }
+        // else
+        // {
+        //     bNewBlock = true;
+        // }
+        // }
+        // else{
+        //     output += lines[i] + '\n';
+        // }
+    }
+    output += "- " + lines[lines.length - 1] + '\n';
+    return output;
+}
