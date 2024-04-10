a43c7eb937f63f3a4f89d0140bf2b6b8d256f133
Bo Li <lb284542@gmail.com>
2024-04-10 19:53:14 +0800

    fix euquation matching

--- ToLogseq.js
+++ ToLogseq.js
@@ -2,7 +2,7 @@
 // @name         ToLogseq Format Converter for ChatGPT
 // @name:zh-CN   ToLogseq Markdown格式转换器 for ChatGPT
 // @namespace    http://tampermonkey.net/
-// @version      0.1.10
+// @version      0.1.12
 // @description  Convert markdown text to Logseq formatted Markdown text, which is available for ChatGPT and other similar tools using md format.
 // @description:zh-cn 将 Markdown 文本转换为 Logseq 格式的 Markdown 文本，可用于 ChatGPT 和其他使用 md 格式的类似工具。
 // @author       Another_Ghost
@@ -21,7 +21,7 @@
     const button = document.createElement('button');
     button.textContent = 'ToLogseq'; // Text displayed on the button
     button.style.position = 'fixed'; // Make button position fixed
-    button.style.bottom = '95px';    // Distance from the bottom of the viewport
+    button.style.bottom = '40px';    // Distance from the bottom of the viewport
     button.style.right = '20px';     // Distance from the right of the viewport
     button.style.zIndex = '1000';    // Ensure the button is on top of other elements
 
@@ -37,7 +37,7 @@
             const clipboardText = await navigator.clipboard.readText(); // Read text from clipboard
             if(clipboardText.includes(mark))
             {
-                showAlert('Already converted to Logseq!', 'orange');
+                showAlert('Already converted to Logseq!', 'lightorange');
             }
             else
             {
@@ -56,9 +56,9 @@
         const alertBox = document.createElement('div');
         alertBox.textContent = message;
         alertBox.style.position = 'fixed';
-        alertBox.style.bottom = '130px';
+        alertBox.style.bottom = '60px';
         alertBox.style.right = '20px';
-        alertBox.style.backgroundColor = backgroundColor;
+        alertBox.style.backgroundColor = 'lightyellow';
         alertBox.style.padding = '10px';
         alertBox.style.borderRadius = '5px';
         alertBox.style.zIndex = '1001';
@@ -108,14 +108,15 @@ function ChatGPTToLogseq(text) {
         //     bNewBlock = false;
         // }
 
-        if(lines[i].trim().match(/^\\[\(\[]/) && lines[i].trim().match(/\\[\)\]]$/))
+        if(lines[i].trim().match(/^\\\[/) && lines[i].trim().match(/\\\])$/))
+        {
+            lines[i] = lines[i].trim().replace(/^\\\[ ?/, '$$').replace(/\\\] ?$/, '$$');
+        }
+        else if(lines[i].trim().match(/^\\\(/) && lines[i].trim().match(/\\\)$/))
         {
             lines[i] = lines[i].trim().replace(/^\\\(/, '$$').replace(/\\\)$/, '$$');
         }
-        if(lines[i].trim() !== '')
-        {
-            output += lines[i] + '\n';
-        }
+
         // else
         // {
         //     bNewBlock = true;
